ca005fd24b18fec45403599c41f64f765db6b7d8
Luca Scalzotto <luca@scorpiac.com>
2017-07-29 22:03:59 +0200

    Add scripts for hexagon tiles

---
+++ Assets/Scripts/HexagonDirection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public enum HexagonDirection
+{
+	TOP_LEFT,
+	TOP_RIGHT,
+	LEFT,
+	RIGHT,
+	BOTTOM_LEFT,
+	BOTTOM_RIGHT
+}
---
+++ Assets/Scripts/HexagonTile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexagonTile : MonoBehaviour
+{
+	private Dictionary<HexagonDirection, HexagonTile> neighbors = new Dictionary<HexagonDirection, HexagonTile>();
+
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+
+	}
+
+	public HexagonTile this[HexagonDirection direction]
+	{
+		get => neighbors[direction];
+		set { neighbors[direction] = value; }
+	}
+}
