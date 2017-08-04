a966fda8b08f3a567859c2e7d2b2b73c0763530c
Luca Scalzotto <luca@scorpiac.com>
2017-08-04 22:17:17 +0200

    Improve PlayingFieldController

--- Assets/Scripts/PlayingFieldController.cs
+++ Assets/Scripts/PlayingFieldController.cs
@@ -6,30 +6,50 @@ public class PlayingFieldController : MonoBehaviour
 {
 	public GameObject tilePrefab;
 
-	private GameObject tiles;
+	private GameObject tilesContainer;
+
+	// Columns, then rows.
+	private readonly Dictionary<int, Dictionary<int, GameObject>> columns = new Dictionary<int, Dictionary<int, GameObject>>();
 
 	public void Start()
 	{
-		tiles = gameObject.transform.Find("Tiles").gameObject;
-		GenerateTiles();
-	}
+		tilesContainer = gameObject.transform.Find("Tiles").gameObject;
 
-	public HexagonTile GetTile(int x, int y)
-	{
-		Transform child = tiles.transform.Find(x + "," + y);
-		return child == null ? null : child.gameObject.GetComponent<HexagonTile>();
-	}
-
-	private void GenerateTiles()
-	{
+		// Generate some tiles.
 		for (int x = -5; x < 5; x++)
 			for (int y = -5; y < 5; y++)
-			{
-				GameObject newTile = Instantiate(tilePrefab, tiles.transform);
-				newTile.name = x.ToString() + ',' + y;
-				HexagonTile tile = newTile.GetComponent<HexagonTile>();
-				tile.TileX = x;
-				tile.TileZ = y;
-			}
+				CreateTile(new Point(x, y));
+	}
+
+	public GameObject CreateTile(Point p)
+	{
+		// Create the gameobject.
+		GameObject newTile = Instantiate(tilePrefab, tilesContainer.transform);
+		newTile.name = p.ToString();
+
+		// Set the tile position for the script.
+		HexagonTile tile = newTile.GetComponent<HexagonTile>();
+		tile.TileX = p.X;
+		tile.TileZ = p.Y;
+
+		// Save the tile.
+		if (!columns.ContainsKey(p.X))
+			columns[p.X] = new Dictionary<int, GameObject>();
+		columns[p.X][p.Y] = newTile;
+
+		return newTile;
+	}
+
+	public GameObject this[Point p]
+	{
+		get
+		{
+			Dictionary<int, GameObject> column;
+			GameObject value = null;
+			// Try to get the row, if it exists try to get the tile.
+			if (columns.TryGetValue(p.X, out column))
+				column.TryGetValue(p.Y, out value);
+			return value;
+		}
 	}
 }
---
+++ Assets/Scripts/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Point
+{
+	public Point(int x, int y)
+	{
+		X = x;
+		Y = y;
+	}
+
+	public override string ToString()
+	{
+		return X.ToString() + ',' + Y;
+	}
+
+	public int X { get; set; }
+	public int Y { get; set; }
+}
