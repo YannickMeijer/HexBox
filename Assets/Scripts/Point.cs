55f74c081396fa6257517c02a75b4fd91b292a62
Luca Scalzotto <luca@scorpiac.com>
2017-08-05 23:41:00 +0200

    Add Unit class, improve point and playing field

--- Assets/Scripts/Point.cs
+++ Assets/Scripts/Point.cs
@@ -16,6 +16,20 @@ public class Point
 		return X.ToString() + ',' + Y;
 	}
 
+	public override bool Equals(object obj)
+	{
+		if (obj == null || !(obj is Point))
+			return false;
+
+		Point other = obj as Point;
+		return other.X == X && other.Y == Y;
+	}
+
+	public override int GetHashCode()
+	{
+		return ToString().GetHashCode();
+	}
+
 	public int X { get; set; }
 	public int Y { get; set; }
 }
---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+	public int Defense = 0;
+	public int MaxHealth = 0;
+
+	private int health;
+
+	private void Start()
+	{
+		health = MaxHealth;
+	}
+
+	protected virtual void Die()
+	{
+		Destroy(gameObject);
+	}
+
+	public int Health
+	{
+		get { return health; }
+		set
+		{
+			health = Mathf.Clamp(value, 0, MaxHealth);
+			if (health <= 0)
+				Die();
+		}
+	}
+}
