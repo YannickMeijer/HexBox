d8e84a4d36b5104ceefbe568cdfbcc5113b006ab
Luca Scalzotto <luca@scorpiac.com>
2017-08-07 22:56:01 +0200

    Clean up code a bit

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -2,78 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hand : MonoBehaviour {
+public class Hand : MonoBehaviour
+{
+	public List<Card> cardsInHand;
+	public int cameraDistance, startingHand;
+	private Camera mainCamera;
+	public int clickedCard, cardCount;
+	public float frustumHeight, frustumWidth;
+	public Deck deck;
 
-    public List<Card> cardsInHand;
-    public int cameraDistance, startingHand;
-    private Camera mainCamera;
-    public int clickedCard, cardCount;
-    public float frustumHeight, frustumWidth;
-    public Deck deck;
+	private void Start()
+	{
+		mainCamera = GetComponentInParent<Camera>();
+		clickedCard = -1;
+		startingHand = 3;
+		for (int x = 0; x < cardsInHand.Count; x++)
+		{
+			cardsInHand[x].transform.SetParent(this.transform);
+			cardsInHand[x].transform.rotation = this.transform.rotation;
+			cardsInHand[x].playerHand = this;
+			cardsInHand[x].location = Card.Location.HAND;
+			cardCount++;
+		}
+	}
 
 
-	void Start() {
-        mainCamera = GetComponentInParent<Camera>();
-        clickedCard = -1;
-        startingHand = 3;
-        for (int x = 0; x < cardsInHand.Count; x++)
-        {
-            cardsInHand[x].transform.SetParent(this.transform);
-            cardsInHand[x].transform.rotation = this.transform.rotation;
-            cardsInHand[x].playerHand = this;
-            cardsInHand[x].location = Card.Location.HAND;
-            cardCount++;
-        }
-    }
-	
+	private void Update()
+	{
+		calcFrustum();
+		float frustumWidthDivision = frustumWidth / (cardCount + 1);
+		for (int x = 0; x < cardCount; x++)
+		{
+			cardsInHand[x].UpdatePosition(new Vector3(frustumWidthDivision * (x + 1), 0, 0), frustumHeight, cameraDistance);
+			cardsInHand[x].handPosition = x;
+		}
+	}
 
-	void Update () {
-        calcFrustum();
-        float frustumWidthDivision = frustumWidth / (cardCount + 1);
-        for (int x = 0; x < cardCount; x++)
-        {
-            cardsInHand[x].UpdatePosition(new Vector3( frustumWidthDivision * (x + 1), 0, 0), frustumHeight , cameraDistance);
-            cardsInHand[x].handPosition = x;
-        }
-    }
+	public void UpdatePlayCard(int newCard)
+	{
+		if (clickedCard != -1)
+			cardsInHand[clickedCard].clicked = false;
+		clickedCard = newCard;
+	}
 
-    public void UpdatePlayCard(int newCard)
-    {
-        if (clickedCard != -1)
-        {
-            cardsInHand[clickedCard].clicked = false;
-        }
-        clickedCard = newCard;
-    }
+	public void PlayOnHexagon(HexagonTile targetHex)
+	{
+		Debug.Log("Call Succseful");
 
-    public void PlayOnHexagon(HexagonTile targetHex)
-    {
-        Debug.Log("Call Succseful");
-            
-        if (clickedCard != -1)
-        {
-            cardsInHand[clickedCard].Play(targetHex);
-            
-            for (int x = clickedCard + 1; x < cardCount;x++)
-            {
-                cardsInHand[x - 1] = cardsInHand[x];
-                cardsInHand[x - 1].handPosition = x - 1;
-            }
-            cardCount -= 1;
-            clickedCard = -1;
-        }
-    }
+		if (clickedCard != -1)
+		{
+			cardsInHand[clickedCard].Play(targetHex);
 
-    void DrawCard()
-    {
-        Card temp = deck.Draw();
-    }
+			for (int x = clickedCard + 1; x < cardCount; x++)
+			{
+				cardsInHand[x - 1] = cardsInHand[x];
+				cardsInHand[x - 1].handPosition = x - 1;
+			}
+			cardCount -= 1;
+			clickedCard = -1;
+		}
+	}
 
-    void calcFrustum()
-    {
-        frustumHeight = 2.0f * cameraDistance * Mathf.Tan(mainCamera.fieldOfView * 0.5f * Mathf.Deg2Rad);
-        frustumWidth = mainCamera.aspect * frustumHeight;
-        transform.localPosition = new Vector3(-frustumWidth / 2, -frustumHeight / 2, cameraDistance);
-        //Places the origin of Hand left and down from the camera,at the edge of what's visible
-    }
+	private void DrawCard()
+	{
+		Card temp = deck.Draw();
+	}
+
+	private void calcFrustum()
+	{
+		frustumHeight = 2.0f * cameraDistance * Mathf.Tan(mainCamera.fieldOfView * 0.5f * Mathf.Deg2Rad);
+		frustumWidth = mainCamera.aspect * frustumHeight;
+		transform.localPosition = new Vector3(-frustumWidth / 2, -frustumHeight / 2, cameraDistance);
+		//Places the origin of Hand left and down from the camera,at the edge of what's visible
+	}
 }
--- Assets/Scripts/HexagonTile.cs
+++ Assets/Scripts/HexagonTile.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class HexagonTile : MonoBehaviour
 {
 	public float Radius = 0.5f;
-	public int TileX;
-	public int TileZ;
+
+	[SerializeField]
+	private int tileX;
+	[SerializeField]
+	private int tileZ;
 
 	private float offsetX;
 	private float offsetZ;
 
-    private GameObject playerHand;
-    private Hand hand;
+	private GameObject playerHand;
+	private Hand hand;
 
 
 	private void Start()
@@ -21,25 +24,45 @@ public class HexagonTile : MonoBehaviour
 		// Get the offsets.
 		offsetX = Radius * 1.5f;
 		offsetZ = Radius * Mathf.Sqrt(3);
-        
-        UpdatePosition();
 
-        playerHand = GameObject.FindGameObjectWithTag("Hand");
-        hand = playerHand.GetComponent<Hand>();
-    }
+		UpdatePosition();
+
+		playerHand = GameObject.FindGameObjectWithTag("Hand");
+		hand = playerHand.GetComponent<Hand>();
+	}
 
 	private void UpdatePosition()
 	{
 		// Set the new x and z, keeping the y.
 		gameObject.transform.localPosition = new Vector3(
-			TileX * offsetX,
+			tileX * offsetX,
 			gameObject.transform.localPosition.y,
-			(TileX % 2 == 0 ? TileZ : TileZ + 0.5f) * offsetZ // Z is dependent on the column, odd columns are shifted half a unit up.
+			(tileX % 2 == 0 ? tileZ : tileZ + 0.5f) * offsetZ // Z is dependent on the column, odd columns are shifted half a unit up.
 		);
 	}
 
-    public void OnMouseUpAsButton()
-    {
-        hand.PlayOnHexagon(this);        
-    }
+	public void OnMouseUpAsButton()
+	{
+		hand.PlayOnHexagon(this);
+	}
+
+	public int TileX
+	{
+		get { return tileX; }
+		set
+		{
+			tileX = value;
+			UpdatePosition();
+		}
+	}
+
+	public int TileZ
+	{
+		get { return tileZ; }
+		set
+		{
+			tileZ = value;
+			UpdatePosition();
+		}
+	}
 }
