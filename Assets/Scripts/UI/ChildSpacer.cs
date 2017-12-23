d2fe29e6b93aa4e53f51f9be7d7a811e0c80f6f5
Luca Scalzotto <luca@scorpiac.com>
2017-12-23 15:59:26 +0100

    Add ChildSpacer script for UI buttons

---
+++ Assets/Scripts/UI/ChildSpacer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildSpacer : MonoBehaviour
+{
+    public float Margin;
+
+    private void Start()
+    {
+        float y = GetComponent<RectTransform>().rect.yMax;
+        foreach (Transform child in transform)
+        {
+            // Subtract half the height to align with the top.
+            y -= child.GetComponent<RectTransform>().rect.height / 2;
+
+            // Set the child position.
+            Vector3 childPos = child.localPosition;
+            childPos.y = y;
+            child.localPosition = childPos;
+
+            y -= Margin;
+        }
+    }
+}
