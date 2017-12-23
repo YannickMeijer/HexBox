7abe200a33e18ec058eafe6a41eb89b76085baee
Luca Scalzotto <luca@scorpiac.com>
2017-12-23 16:09:50 +0100

    Make buttons on main menu functional

---
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private const string SCENES_DIR = "scenes/";
+
+    /// <summary>
+    /// Load a scene.
+    /// </summary>
+    /// <param name="scene">The name of the scene to load.</param>
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(SCENES_DIR + scene);
+    }
+
+    /// <summary>
+    /// Exit the game.
+    /// </summary>
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
