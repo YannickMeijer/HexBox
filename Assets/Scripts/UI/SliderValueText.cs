481db891477a33d202533f2510112915bf2ee796
Luca Scalzotto <luca@scorpiac.com>
2017-12-29 13:11:43 +0100

    Add player count value text

---
+++ Assets/Scripts/UI/SliderValueText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// This script sets text based on the value of a slider.
+/// </summary>
+public class SliderValueText : MonoBehaviour
+{
+    public Text ValueText;
+
+    private void Start()
+    {
+        GetComponent<Slider>().onValueChanged.AddListener(value => ValueText.text = value.ToString());
+    }
+}
